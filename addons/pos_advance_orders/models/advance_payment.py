33ba88abcdf987f061c17a49dcfcfcb98c9cfbb4
Anabtawi-Invest <info@anabtawisweets.com>
2026-01-26 15:50:22 +0300

    Update advance_payment.py

--- addons/pos_advance_orders/models/advance_payment.py
+++ addons/pos_advance_orders/models/advance_payment.py
@@ -1,14 +1,29 @@
 from odoo import models, fields, _
 from odoo.exceptions import UserError
 
+
 class PosAdvancePayment(models.Model):
     _name = "pos.advance.payment"
+    _description = "POS Advance Payment"
 
     order_id = fields.Many2one("pos.advance.order", required=True)
-    amount = fields.Monetary(required=True)
-    currency_id = fields.Many2one("res.currency", related="order_id.currency_id")
+
+    company_id = fields.Many2one(
+        "res.company",
+        default=lambda self: self.env.company,
+        required=True,
+    )
+
+    currency_id = fields.Many2one(
+        "res.currency",
+        related="company_id.currency_id",
+        store=True,
+        readonly=True,
+    )
+
+    amount = fields.Monetary(required=True, currency_field="currency_id")
     journal_id = fields.Many2one("account.journal", required=True)
-    move_id = fields.Many2one("account.move")
+    move_id = fields.Many2one("account.move", readonly=True)
 
     def action_post(self):
         self.ensure_one()
@@ -23,18 +38,25 @@ class PosAdvancePayment(models.Model):
             raise UserError(_("Missing Advance Liability Account"))
 
         journal_account = self.journal_id.default_account_id
+        if not journal_account:
+            raise UserError(_("The selected journal has no Default Account."))
 
         move = self.env["account.move"].create({
             "move_type": "entry",
             "journal_id": self.journal_id.id,
+            "company_id": self.company_id.id,
             "line_ids": [
                 (0, 0, {
                     "account_id": journal_account.id,
                     "debit": self.amount,
+                    "credit": 0.0,
+                    "name": _("Advance Payment"),
                 }),
                 (0, 0, {
                     "account_id": liability.id,
+                    "debit": 0.0,
                     "credit": self.amount,
+                    "name": _("Advance Payment"),
                 }),
             ],
         })
