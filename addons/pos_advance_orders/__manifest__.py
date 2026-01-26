b3465dfc88c7e5750b5a36578b928ea19f928e44
Anabtawi-Invest <info@anabtawisweets.com>
2026-01-26 16:21:43 +0300

    Update __manifest__.py

--- addons/pos_advance_orders/__manifest__.py
+++ addons/pos_advance_orders/__manifest__.py
@@ -3,12 +3,25 @@
     "version": "19.0.1.0.0",
     "category": "Point of Sale",
     "summary": "Advance Orders, Pledge Deposits, Accounting Liability",
-    "depends": ["point_of_sale", "account", "contacts", "product"],
+    "depends": [
+        "point_of_sale", 
+        "account", 
+        "contacts", 
+        "product"
+    ],
     "data": [
         "security/security.xml",
         "security/ir.model.access.csv",
         "views/pos_config_views.xml",
         "views/advance_order_views.xml",
     ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_advance_orders/static/src/pos/index.js",
+            "pos_advance_orders/static/src/pos/**/*.xml",
+        ],
+    },
     "installable": True,
+    "application": False,
+    "license": "LGPL-3",
 }
