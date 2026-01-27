23fcd8936a50948cfd79df39eb1da60d4fbd4308
Anabtawi-Invest <info@anabtawisweets.com>
2026-01-27 08:44:22 +0300

    Update __init__.py

--- addons/pos_advance_orders/models/__init__.py
+++ addons/pos_advance_orders/models/__init__.py
@@ -1,6 +1,5 @@
 from . import pos_config
 from . import discount_profile
-from . import pos_advance_order
 from . import pos_advance_order_line
 from . import pos_advance_order_revision
 from . import pos_advance_payment
