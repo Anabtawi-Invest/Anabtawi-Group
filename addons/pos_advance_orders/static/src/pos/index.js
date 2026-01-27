783e740f4549eb7b636e85ead78e870fd00930cd
Anabtawi-Invest <info@anabtawisweets.com>
2026-01-27 11:09:52 +0300

    Update index.js

--- addons/pos_advance_orders/static/src/pos/index.js
+++ addons/pos_advance_orders/static/src/pos/index.js
@@ -1,7 +1,6 @@
 import "./advance_orders_popup";
 import "./advance_orders_button";
 import "./pledge_autoline";
-import "./export_flags";
 import "./receipt_split";
 import "./discount_popup";
 import "./discount_button";
