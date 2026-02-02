eb578edcf7dc600accf21a0e16d23772194276a6
Anabtawi-Invest <info@anabtawisweets.com>
2026-02-02 11:24:05 +0300

    Update __manifest__.py

--- restrict_negative_location/__manifest__.py
+++ restrict_negative_location/__manifest__.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Restrict Negative Stock by Location',
+    'version': '1.0',
+    'author': 'Your Name',
+    'depends': ['stock'],
+    'category': 'Warehouse',
+    'summary': 'Restrict negative stock based on location checkbox',
+    'description': 'Adds checkbox on locations to restrict negative stock when checked.',
+    'data': [
+        # No views provided here, but you can add one to expose the checkbox
+    ],
+    'installable': True,
+    'application': False,
+}
 
