68e577ec7fa7147a26fc888e222601fab4cbbbe1
Anabtawi-Invest <info@anabtawisweets.com>
2026-02-09 21:40:37 +0300

    Update __manifest__.py

--- Negative Stock Blocking/addons/stock_location_negative_block/__manifest__.py
+++ Negative Stock Blocking/addons/stock_location_negative_block/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Restrict Negative Stock by Location",
+    "name": "Ameen-Negative Stock",
     "version": "19.0.1.0.0",
     "category": "Inventory",
     "summary": "Block negative stock when source location is flagged",
