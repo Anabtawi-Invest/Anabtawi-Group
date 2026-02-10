ccf08c393d7aabe4527ea88c29223e41d4383797
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-10 12:37:17 +0300

    Update hr_payslip_cron.py

--- lateness_reconciliation/models/hr_payslip_cron.py
+++ lateness_reconciliation/models/hr_payslip_cron.py
@@ -1,11 +1,9 @@
 from odoo import models
 
-
 class HrPayslip(models.Model):
-_inherit = 'hr.payslip'
+    _inherit = 'hr.payslip'
 
-
-def _cron_auto_compute_sheet(self):
-slips = self.search([('state', '=', 'draft')])
-for slip in slips:
-slip.compute_sheet()
+    def _cron_auto_compute_sheet(self):
+        slips = self.search([('state', '=', 'draft')])
+        for slip in slips:
+            slip.compute_sheet()
