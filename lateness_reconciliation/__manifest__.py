6664d2c542f25ec25c92e0c643c2fcc7d4e5f007
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-10 12:42:17 +0300

    Update __manifest__.py

--- lateness_reconciliation/__manifest__.py
+++ lateness_reconciliation/__manifest__.py
@@ -4,14 +4,15 @@
 "category": "Human Resources",
 "depends": ["hr_payroll", "hr_attendance", "planning", "mail"],
 "data": [
-"data/salary_input_type.xml",
-"views/hr_payslip_form_view.xml",
-"views/hr_payslip_fields_view.xml",
-"views/hr_payslip_list_view.xml",
-"views/hr_employee_view.xml",
-"data/lateness_cron.xml",
-"data/mass_reconcile_action.xml"
+    "data/salary_input_type.xml",
+    "data/lateness_cron.xml",
+    "data/mass_reconcile_action.xml",
+    "views/hr_payslip_form_view.xml",
+    "views/hr_payslip_fields_view.xml",
+    "views/hr_payslip_list_view.xml",
+    "views/hr_employee_view.xml",
 ],
+
 "installable": True,
 "application": False
 }
