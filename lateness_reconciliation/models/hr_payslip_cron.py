2407693c51827dfba52e9c91ca990b89e0c903d8
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-11 22:50:35 +0300

    Delete lateness_reconciliation directory

--- lateness_reconciliation/models/hr_payslip_cron.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class HrPayslip(models.Model):
-    _inherit = 'hr.payslip'
-
-    def _cron_auto_compute_sheet(self):
-        slips = self.search([('state', '=', 'draft')])
-        for slip in slips:
-            slip.compute_sheet()
