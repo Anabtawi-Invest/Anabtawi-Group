b6a4413866735a89a9b2794dfcf7501bbe32dbe4
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-14 00:01:25 +0300

    Add files via upload

---
+++ lateness_reconciliation/models/hr_payslip_run.py
@@ -0,0 +1,61 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class HrPayslipRun(models.Model):
+    _inherit = "hr.payslip.run"
+
+    total_lateness = fields.Float(compute="_compute_totals")
+    total_overtime = fields.Float(compute="_compute_totals")
+    total_remaining = fields.Float(compute="_compute_totals")
+    coverage_pct = fields.Float(compute="_compute_totals")
+
+    @api.depends(
+        "slip_ids.lateness_hours",
+        "slip_ids.ot_hours_total",
+        "slip_ids.lateness_remaining",
+    )
+    def _compute_totals(self):
+        for run in self:
+            slips = run.slip_ids
+            total_lateness = sum(slips.mapped("lateness_hours"))
+            total_overtime = sum(slips.mapped("ot_hours_total"))
+            total_remaining = sum(slips.mapped("lateness_remaining"))
+
+            run.total_lateness = total_lateness
+            run.total_overtime = total_overtime
+            run.total_remaining = total_remaining
+
+            if total_lateness > 0:
+                run.coverage_pct = ((total_lateness - total_remaining) / total_lateness) * 100
+            else:
+                run.coverage_pct = 100.0
+
+    def action_mass_reconcile_lateness_enterprise(self):
+        """Mass reconcile action that works from:
+        - Pay Run (hr.payslip.run) form action
+        - Payslips list action (active_ids = payslips)
+        """
+        active_model = self.env.context.get("active_model")
+        active_ids = self.env.context.get("active_ids", [])
+
+        slips = self.env["hr.payslip"]
+
+        if active_model == "hr.payslip" and active_ids:
+            slips = slips.browse(active_ids)
+        else:
+            # Called from Pay Run form
+            slips = self.slip_ids
+
+        if not slips:
+            raise UserError(_("No payslips found to reconcile."))
+
+        slips = slips.filtered(lambda s: s.state == "draft")
+
+        for slip in slips:
+            if hasattr(slip, "_lateness_reconcile_for_slip"):
+                slip._lateness_reconcile_for_slip()
+
+        slips._recompute_dashboard_fields()
+
+        return {"type": "ir.actions.client", "tag": "reload"}
