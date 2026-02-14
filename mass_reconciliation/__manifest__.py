1625644f94df4f4ec4de62a2fbcb032333160669
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-14 13:38:45 +0300

    Add files via upload

---
+++ mass_reconciliation/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'كمتلة Mass Reconciliation (Lateness Coverage)',
+    'version': '19.0.1.0.1',
+    'summary': 'Cover lateness using OT buckets then Annual Leave (hours) then remaining lateness for payroll deduction. No OT Bank.',
+    'category': 'Human Resources/Payroll',
+    'author': 'Softobia',
+    'license': 'LGPL-3',
+    'depends': ['hr_payroll', 'hr_holidays'],
+    'data': [
+        'data/payslip_input_type.xml',
+        'security/ir.model.access.csv',
+        'views/hr_payslip_views.xml',
+        'data/server_actions.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
