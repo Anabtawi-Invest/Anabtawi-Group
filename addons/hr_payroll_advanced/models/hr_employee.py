677029c0d1c72d38a441bc0e44b3bc20b505d29e
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-15 20:34:22 +0300

    Update hr_employee.py

--- addons/hr_payroll_advanced/models/hr_employee.py
+++ addons/hr_payroll_advanced/models/hr_employee.py
@@ -4,18 +4,16 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-   overtime_bank_balance = fields.Float(
-    string="OT Bank Balance (OT_TOTAL)",
-    compute="_compute_overtime_bank_balance",
-)
+    overtime_bank_balance = fields.Float(
+        string="OT Bank Balance (OT_TOTAL)",
+        compute="_compute_overtime_bank_balance",
+    )
 
     def _compute_overtime_bank_balance(self):
         for emp in self:
-            lines = self.env["hr.payslip.line"].search(
-                [
-                    ("employee_id", "=", emp.id),
-                    ("code", "=", "OT_TOTAL"),
-                    ("slip_id.state", "=", "done"),
-                ]
-            )
+            lines = self.env["hr.payslip.line"].search([
+                ("employee_id", "=", emp.id),
+                ("code", "=", "OT_TOTAL"),
+                ("slip_id.state", "=", "done"),
+            ])
             emp.overtime_bank_balance = sum(lines.mapped("total"))
