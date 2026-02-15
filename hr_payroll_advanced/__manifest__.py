6f3927cfd0728894c4b830c7b6e7ddc3435dba1d
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-16 00:39:36 +0300

    Add files via upload

---
+++ hr_payroll_advanced/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "HR Payroll Advanced (OT/Late Reconciliation)",
+    "version": "19.0.1.0.0",
+    "category": "Human Resources/Payroll",
+    "summary": "Enterprise-safe lateness reconciliation: OT bank -> Annual Leave -> Unpaid, with dashboards on Payslip & Payrun.",
+    "author": "Anabtawi Group",
+    "license": "LGPL-3",
+    "depends": ["hr_payroll", "hr_holidays"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/res_config_settings_views.xml",
+        "views/hr_employee_ot_bank_views.xml",
+        "views/hr_payslip_views.xml",
+        "views/hr_payslip_run_views.xml",
+        "data/salary_rules.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ hr_payroll_advanced/models/__init__.py
@@ -0,0 +1,4 @@
+from . import res_config_settings
+from . import hr_employee_ot_bank
+from . import hr_payslip
+from . import hr_payslip_run
