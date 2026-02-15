78be2943a9c019584dd1a4e20be509dfff7adb3d
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-15 20:24:58 +0300

    Create __manifest__.py

---
+++ addons/hr_payroll_advanced/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "HR Payroll Advanced (OT/Late Reconciliation)",
+    "version": "19.0.1.0",
+    "depends": ["hr_payroll", "hr_holidays"],
+    "data": [
+        "views/hr_payslip_views.xml",
+        "views/hr_employee_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
