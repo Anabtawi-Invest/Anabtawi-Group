5814d1bcd5850d6cd9417207c2ed33b8f48b7df9
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-16 16:01:35 +0300

    Delete hr_pce_reconciliation directory

--- hr_pce_reconciliation/models/hr_work_entry.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-class HrWorkEntry(models.Model):
-    _inherit = "hr.work.entry"
-
-    pce_reconciled = fields.Boolean(string="PCE Reconciled", default=False, copy=False)
-    pce_reconciled_payslip_id = fields.Many2one("hr.payslip", string="Reconciled Payslip", copy=False)
