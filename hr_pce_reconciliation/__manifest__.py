6f17a934174b10780aff6a882e03735787704ed4
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-16 16:02:08 +0300

    Add files via upload

---
+++ hr_pce_reconciliation/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "PCE Reconciliation (Button + Smart Review) - Odoo 19 Enterprise Safe",
+    "version": "19.2.2.1.0",
+    "category": "Human Resources/Payroll",
+    "summary": "Separate reconciliation button: OT unpaid bank, lateness offsets OT->Annual->Salary input. Smart Review screen with badges on Payrun.",
+    "author": "Anabtawi Group",
+    "license": "LGPL-3",
+    "depends": ["hr_payroll", "hr_holidays", "hr_work_entry"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "data/pce_input_type.xml",
+        "views/hr_payslip_views.xml",
+        "views/hr_payslip_run_views.xml",
+        "views/hr_pce_review_views.xml",
+        "views/hr_employee_views.xml"
+    ],
+    "installable": True,
+    "application": False
+}
