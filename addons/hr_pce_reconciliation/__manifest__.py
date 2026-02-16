545a62555e51a6ccb378c8485a13ce3ecdde0940
Anabtawi-group <malnsour@anabtawisweets.com>
2026-02-16 21:36:35 +0300

    Update __manifest__.py

--- addons/hr_pce_reconciliation/__manifest__.py
+++ addons/hr_pce_reconciliation/__manifest__.py
@@ -1,12 +1,22 @@
 {
-    "name": "Payroll Reconciliation EngineLESS" ,
+    "name": "Payroll Reconciliation EngineLESS",
     "version": "19.6.0",
-    "depends": ["hr_payroll"],
+    "summary": "Automatic lateness reconciliation using OT Total + Annual Leave + Salary Inputs",
+    "category": "Human Resources/Payroll",
+    "author": "Anabtawi Group",
+    "license": "OEEL-1",
+
+    "depends": [
+        "hr_payroll",
+    ],
+
     "data": [
         "security/ir.model.access.csv",
         "views/menu.xml",
         "views/smart_review_views.xml",
     ],
-    "installable": True,
-}
 
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
