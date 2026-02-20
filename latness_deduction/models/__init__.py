6ff833879c98b8ce922e0cf34421373f344fc97a
rana-faris <ranafaris90@gmail.com>
2026-02-20 20:50:47 +0300

    [ADD]price_list

--- latness_deduction/models/__init__.py
+++ latness_deduction/models/__init__.py
@@ -1,5 +1,5 @@
-from . import hr_employee
 from . import hr_payslip
-from . import res_company
 from . import hr_payslip_run
+from . import res_company
 from . import res_config_settings
+from . import hr_employee
