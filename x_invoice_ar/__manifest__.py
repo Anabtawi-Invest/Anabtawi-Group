6712870ea01683cb21f68dc9ac09856e319457e7
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-01 13:02:25 +0300

    Update __manifest__.py

--- x_invoice_ar/__manifest__.py
+++ x_invoice_ar/__manifest__.py
@@ -1,1 +1,10 @@
-
+{
+    "name": "Invoice Arabic Wrapper",
+    "version": "1.0.0",
+    "depends": ["account"],
+    "data": [
+        "report/report_invoice_ar_templates.xml",
+    ],
+    
+    "installable": True,
+}
