aa0a9da45782d0b2c87f0ea80c9cf51f628c740a
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-01 13:28:13 +0300

    Update report_delivery_ar.py

--- x_invoice_ar/report/report_delivery_ar.py
+++ x_invoice_ar/report/report_delivery_ar.py
@@ -1,1 +1,5 @@
+from odoo import models
 
+class ReportDeliveryArabic(models.AbstractModel):
+    _name = "report.x_invoice_ar.report_delivery_ar_wrapper"
+    _inherit = "report.stock.report_delivery"
