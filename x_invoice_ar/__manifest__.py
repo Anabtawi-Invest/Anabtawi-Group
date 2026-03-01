a96dc234af76c7e1aae88709e65ef284a71c50f2
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-01 13:09:23 +0300

    Update __manifest__.py

--- x_invoice_ar/__manifest__.py
+++ x_invoice_ar/__manifest__.py
@@ -1,1 +1,9 @@
-
+{
+    "name": "Invoice Arabic Report",
+    "version": "1.0",
+    "depends": ["account"],
+    "data": [
+        "report/report_invoice_ar_templates.xml",
+    ],
+    "installable": True,
+}
