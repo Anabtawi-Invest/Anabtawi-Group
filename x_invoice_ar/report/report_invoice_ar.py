2f0bba7c4231c84d81f03484ac8b3dfec9b6843c
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-01 14:07:48 +0300

    Update report_invoice_ar.py

--- x_invoice_ar/report/report_invoice_ar.py
+++ x_invoice_ar/report/report_invoice_ar.py
@@ -3,3 +3,4 @@ from odoo import models
 class ReportInvoiceArabic(models.AbstractModel):
     _name = "report.x_invoice_ar.invoice_ar"
     _inherit = "report.account.report_invoice"
+    _description = "Arabic Invoice Report"
