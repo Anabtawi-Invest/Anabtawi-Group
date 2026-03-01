6d89d139b9fb4dd82cbc71df35f52052849f597a
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-01 14:13:42 +0300

    Delete x_invoice_ar/report/report_delivery_ar.py

--- x_invoice_ar/report/report_delivery_ar.py
+++
@@ -1,6 +0,0 @@
-from odoo import models
-
-class ReportDeliveryArabic(models.AbstractModel):
-    _name = "report.x_invoice_ar.delivery_ar"
-    _inherit = "report.stock.report_deliveryslip"
-    _description = "Arabic Delivery Slip Report"
