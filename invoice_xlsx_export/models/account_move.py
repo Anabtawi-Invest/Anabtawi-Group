51970792c8525f70913f490137781cca747fb493
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-02 14:51:31 +0300

    Update account_move.py

--- invoice_xlsx_export/models/account_move.py
+++ invoice_xlsx_export/models/account_move.py
@@ -1,1 +1,20 @@
+from odoo import models
 
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def action_export_xlsx_en(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_url",
+            "url": f"/invoice_xlsx_export/{self.id}?lang=en_US",
+            "target": "self",
+        }
+
+    def action_export_xlsx_ar(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_url",
+            "url": f"/invoice_xlsx_export/{self.id}?lang=ar_001",
+            "target": "self",
+        }
