ac78329f01adfc34a540323402997b9ffce50c4d
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-02 14:50:58 +0300

    Update __manifest__.py

--- invoice_xlsx_export/__manifest__.py
+++ invoice_xlsx_export/__manifest__.py
@@ -1,1 +1,9 @@
-
+{
+    "name": "Invoice XLSX Export",
+    "version": "19.0.1.0.0",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
