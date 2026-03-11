ee33c325b28529a300107693aee25386499080c7
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-11 23:08:11 +0300

    Update __manifest__.py

--- pos_pledge/__manifest__.py
+++ pos_pledge/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'POS Pledge (Rahn) Management-anabtawi',
+    'name': '(do not download)POS Pledge (Rahn) Management-anabtawi',
     'version': '19.0.1.0.0',
     'category': 'Point of Sale',
     'summary': 'Manage pledge (Rahn) scenarios with employees, delivery, and accounting',
