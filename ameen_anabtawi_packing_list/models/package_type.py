1e6dc97334924eb1fd59127d59f243c603f4c992
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-30 13:50:54 +0300

    Update package_type.py

--- ameen_anabtawi_packing_list/models/package_type.py
+++ ameen_anabtawi_packing_list/models/package_type.py
@@ -1,1 +1,10 @@
+# models/package_type.py
+from odoo import models, fields
 
+class PackingPackageType(models.Model):
+    _name = "packing.package.type"
+    _description = "Packing Package Type"
+    _order = "name"
+
+    name = fields.Char(required=True, translate=True)
+    active = fields.Boolean(default=True)
