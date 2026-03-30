ef6ab11affe4292c0acbbf533ded53b92e71ade9
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-30 13:51:35 +0300

    Update sale_order_line.py

--- ameen_anabtawi_packing_list/models/sale_order_line.py
+++ ameen_anabtawi_packing_list/models/sale_order_line.py
@@ -1,1 +1,13 @@
+# models/sale_order_line.py
+from odoo import models, fields, api
 
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    x_qty_per_carton = fields.Integer(string="Qty per Carton", default=0)
+
+    @api.onchange("product_id")
+    def _onchange_product_id_set_qty_per_carton(self):
+        for line in self:
+            if line.product_id:
+                line.x_qty_per_carton = line.product_id.product_tmpl_id.x_qty_per_carton or 0
