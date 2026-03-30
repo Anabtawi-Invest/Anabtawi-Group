ad9ec5330d13f892a00d0d96b92da9882f9fb744
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-30 13:46:26 +0300

    Update __manifest__.py

--- ameen_anabtawi_packing_list/__manifest__.py
+++ ameen_anabtawi_packing_list/__manifest__.py
@@ -1,1 +1,18 @@
-
+# __manifest__.py
+{
+    "name": "Ameen_Anabtawi_Packing List",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Packing List PDF from Customer Invoices + product logistics fields",
+    "depends": ["account", "sale", "stock", "product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/package_type_menu.xml",
+        "views/product_template_view.xml",
+        "views/sale_order_view.xml",
+        "report/packing_list_report.xml",
+        "report/packing_list_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
