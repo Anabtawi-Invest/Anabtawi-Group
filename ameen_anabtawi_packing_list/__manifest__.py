b939e3752586b01c58e32028ac5cd4b04a280ff0
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-31 13:29:44 +0300

    Update __manifest__.py

--- ameen_anabtawi_packing_list/__manifest__.py
+++ ameen_anabtawi_packing_list/__manifest__.py
@@ -5,18 +5,14 @@
     "summary": "Two Packing List reports (with & without dates) + logistics fields",
     "depends": ["account", "sale", "stock", "product"],
     "data": [
-        # Security (needed to create/select Package Types)
         "security/ir.model.access.csv",
 
-        # Views
         "views/package_type_menu.xml",
         "views/product_template_view.xml",
         "views/sale_order_view.xml",
 
-        # Report Actions (2 reports)
         "report/packing_list_report.xml",
 
-        # QWeb templates (body first)
         "report/packing_list_body.xml",
         "report/packing_list_template.xml",
         "report/packing_list_template_with_dates.xml",
