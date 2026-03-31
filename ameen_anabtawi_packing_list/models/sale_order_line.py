25339fbfe12ae460d3b0bf9a089d1ddd18c1c63d
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-31 13:01:27 +0300

    Update sale_order_line.py

--- ameen_anabtawi_packing_list/models/sale_order_line.py
+++ ameen_anabtawi_packing_list/models/sale_order_line.py
@@ -5,6 +5,10 @@ class SaleOrderLine(models.Model):
 
     x_qty_per_carton = fields.Integer(string="Qty per Carton", default=0)
 
+    # NEW: production & expiry on SO line
+    x_production_date = fields.Date(string="Production Date")
+    x_expiry_date = fields.Date(string="Expiry Date")
+
     @api.onchange("product_id")
     def _onchange_product_id_set_qty_per_carton(self):
         for line in self:
