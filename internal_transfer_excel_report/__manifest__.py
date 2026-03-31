afcb89fe5d8991e6706daa86619645c94397594e
Ameen_Anabtawi <info@anabtawisweets.com>
2026-03-31 15:53:25 +0300

    Add files via upload

---
+++ internal_transfer_excel_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Internal Transfer Excel Report-Anabtawi',
+    'version': '1.0',
+    'depends': ['stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/wizard_view.xml',
+    ],
+    'installable': True,
+}
