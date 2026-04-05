ff0599746c69b860422d34731d5b3c4aa5adb5af
Ameen_Anabtawi <info@anabtawisweets.com>
2026-04-05 09:11:41 +0300

    Create __manifest.py

---
+++ negative stock blocking/__manifest.py
@@ -0,0 +1,15 @@
+{
+    "name": "Negative Stock",
+    "author":"Ameen Arabiyat",
+  "version": "19.0.1.0.0",
+    "category": "Inventory",
+    "summary": "Block negative stock when source location is flagged",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_location_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
+
