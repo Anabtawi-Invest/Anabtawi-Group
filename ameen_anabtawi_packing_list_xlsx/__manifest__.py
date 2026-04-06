dfea5e69fa5c2660e50ecd56bdd0243416cd34c6
Ameen_Anabtawi <info@anabtawisweets.com>
2026-04-06 09:09:36 +0300

    Update __manifest__.py

--- ameen_anabtawi_packing_list_xlsx/__manifest__.py
+++ ameen_anabtawi_packing_list_xlsx/__manifest__.py
@@ -1,24 +1,15 @@
 {
-    "name": "Ameen Packing List",
+    "name": "Ameen Packing List XLSX",
     "version": "19.0.1.0.0",
     "category": "Accounting",
-    "author": "Ameen Arabiyat",
-    "summary": "Two Packing List reports (with & without dates) + logistics fields + XLSX export (Action menu)",
-    "depends": ["account", "sale", "stock", "product", "web"],
+    "summary": "Packing List XLSX export (Action menu)",
+    "depends": [
+        "ameen_anabtawi_packing_list",
+        "account",
+        "web",
+    ],
     "data": [
-       
-
-        "views/package_type_menu.xml",
-        "views/product_template_view.xml",
-        "views/sale_order_view.xml",
-
-        "report/packing_list_report.xml",
-        "report/packing_list_body.xml",
-        "report/packing_list_template.xml",
-        "report/packing_list_template_with_dates.xml",
-
-        # NEW: Action menu XLSX export
-        "views/packing_list_xlsx_actions.xml",
+        "report/packing_list_xlsx_actions.xml",
     ],
     "installable": True,
     "application": False,
