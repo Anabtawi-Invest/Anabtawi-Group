7f51ee6210e95fa08d41ffd6b9284fc9f3988a38
Ameen_Anabtawi <info@anabtawisweets.com>
2026-04-06 08:31:37 +0300

    Create __manifest__.py

---
+++ ameen_anabtawi_packing_list_xlsx/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Ameen Packing List XLSX",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "author": "Ameen Arabiyat",
+    "summary": "Export Packing List to XLSX (with & without dates)",
+    "depends": [
+        "ameen_anabtawi_packing_list",
+        "report_xlsx",
+        "account",
+    ],
+    "data": [
+        "report/packing_list_xlsx_actions.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
