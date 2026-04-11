7e088b4618074120bca67ddc546fedf59fc49bf0
rana-faris <erp-odoo@anabtawisweets.com>
2026-04-11 09:27:57 +0300

    [ِADD]Zkteco

--- portal_check_in/__manifest__.py
+++ portal_check_in/__manifest__.py
@@ -7,11 +7,15 @@
     'category': 'Portal',
     'author': "enbtawi",
     'version': '1.2',
-    'depends': ['base', 'hr', 'hr_payroll', 'base_portal', 'hr_holidays', 'resource', 'portal', 'hr_attendance'],
+    'depends': [
+        'base', 'hr', 'hr_payroll', 'base_portal', 'hr_holidays',
+        'resource', 'portal', 'hr_attendance', 'hr_attendance_geofence_config',
+    ],
     'data': [
         'security/ir.model.access.csv',
         'views/res_users_portal_employee.xml',
         'views/hr_employee_user_link.xml',
+        'views/hr_employee_attendance_location.xml',
         'views/portal_check_in_templates.xml',
     ],
 
