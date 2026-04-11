09d21830210b2a6de4d859be9f49bd956d94b030
Ameen_Anabtawi <info@anabtawisweets.com>
2026-04-11 13:42:13 +0300

    Add files via upload

---
+++ partner_vendor_customer_flag/models/account_move.py
@@ -0,0 +1,50 @@
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.constrains("partner_id", "move_type")
+    def _check_partner_vendor_customer_flags(self):
+        for move in self:
+            if not move.partner_id:
+                continue
+
+            # Vendor documents
+            if move.move_type in ("in_invoice", "in_refund", "in_receipt"):
+                if not move.partner_id.x_is_vendor:
+                    raise ValidationError(_(
+                        "You can only select a contact marked as 'Is Vendor' for Vendor Bills/Credits."
+                    ))
+
+            # Customer documents
+            if move.move_type in ("out_invoice", "out_refund", "out_receipt"):
+                if not move.partner_id.x_is_customer:
+                    raise ValidationError(_(
+                        "You can only select a contact marked as 'Is Customer' for Customer Invoices/Credit Notes."
+                    ))
+
+    @api.onchange("partner_id", "move_type")
+    def _onchange_partner_vendor_customer_flags(self):
+        for move in self:
+            if not move.partner_id:
+                continue
+
+            if move.move_type in ("in_invoice", "in_refund", "in_receipt") and not move.partner_id.x_is_vendor:
+                move.partner_id = False
+                return {
+                    "warning": {
+                        "title": _("Not allowed"),
+                        "message": _("This is a Vendor Bill/Credit. Please select a contact marked as 'Is Vendor'."),
+                    }
+                }
+
+            if move.move_type in ("out_invoice", "out_refund", "out_receipt") and not move.partner_id.x_is_customer:
+                move.partner_id = False
+                return {
+                    "warning": {
+                        "title": _("Not allowed"),
+                        "message": _("This is a Customer Invoice/Credit. Please select a contact marked as 'Is Customer'."),
+                    }
+                }
