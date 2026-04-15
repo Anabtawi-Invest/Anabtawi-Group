30b430312092bc22cfc99a1e85d0fa85ed6a0bc5
rana-faris <erp-odoo@anabtawisweets.com>
2026-04-15 21:20:05 +0300

    [ADD]sale discount approval

---
+++ ag_sale_line_approval/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Sale Line Approval",
+    "version": "19.0.1.0.0",
+    "category": "Sales",
+    "summary": "Per-line sale approvals for discounts and price overrides.",
+    "depends": ["sale", "sale_stock", "approvals", "mail"],
+    "data": [
+        "views/approval_category_views.xml",
+        "views/approval_request_views.xml",
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
