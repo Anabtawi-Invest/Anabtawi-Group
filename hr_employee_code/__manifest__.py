e1b1cfb68cef2d4e12c77d3a811245fe3df48357
rana-faris <erp-odoo@anabtawisweets.com>
2026-04-18 10:51:45 +0300

    [ADD]employee_code_number

---
+++ hr_employee_code/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'hr.payslip_enhancement',
+    'version': '19.0.1.0.4',
+    'summary': 'Cover lateness using OT buckets then Annual Leave (hours) then remaining lateness for payroll deduction. No OT Bank.',
+    'category': 'Human Resources/Payroll',
+    'author': 'Rana Faris',
+    'license': 'LGPL-3',
+    'depends': ['lateness_company_settings1', 'hr_payroll', 'hr_holidays', 'planning'],
+    'data': [
+        'views/hr_employee.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
