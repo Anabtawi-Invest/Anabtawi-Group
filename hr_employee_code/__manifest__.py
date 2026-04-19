ea0789a450efc679c9394371acf2b2d0487bd2d1
Ameen_Anabtawi <info@anabtawisweets.com>
2026-04-19 13:42:38 +0300

    Update __manifest__.py

--- hr_employee_code/__manifest__.py
+++ hr_employee_code/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '19.0.1.0.4',
     'summary': 'Cover lateness using OT buckets then Annual Leave (hours) then remaining lateness for payroll deduction. No OT Bank.',
     'category': 'Human Resources/Payroll',
-    'author': 'Rana Faris',
+    "author":"Anabtawi",
     'license': 'LGPL-3',
     'depends': ['lateness_company_settings1', 'hr_payroll', 'hr_holidays', 'planning'],
     'data': [
