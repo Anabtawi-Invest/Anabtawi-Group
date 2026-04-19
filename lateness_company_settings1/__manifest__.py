88a7ced03db4fa4eeed598ada0b8ad20797b7f67
Ameen_Anabtawi <info@anabtawisweets.com>
2026-04-19 13:44:20 +0300

    Update __manifest__.py

--- lateness_company_settings1/__manifest__.py
+++ lateness_company_settings1/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '19.0.1.0',
     'summary': 'Adds lateness & OT settings fields to res.company. Install this first; used by lateness deduction.',
     'category': 'Human Resources',
-    'author': 'Rana Faris',
+    "author":"Anabtawi",
     'license': 'LGPL-3',
     'depends': ['base', 'hr_holidays'],
     'data': [],
