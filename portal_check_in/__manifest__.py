d718282d03c3576b6c8a432b0e938797fb6fd669
rana-faris <erp-odoo@anabtawisweets.com>
2026-04-25 23:30:10 +0300

    [MOD]hr_attendance_weekly_overtime_eligibility

--- portal_check_in/__manifest__.py
+++ portal_check_in/__manifest__.py
@@ -6,19 +6,19 @@
     """,
     'category': 'Portal',
     'author': "enbtawi",
-    'version': '1.4',
+    'version': '1.5',
     'depends': [
-        'base', 'hr', 'hr_payroll', 'base_portal', 'hr_holidays','resource', 'portal', 'hr_attendance','hr_attendance_overtime_approval_bridge'
+        'base', 'hr', 'hr_payroll', 'base_portal', 'hr_holidays',
+        'resource', 'portal', 'hr_attendance', 'hr_attendance_geofence_config',
+        'hr_attendance_overtime_approval_bridge',
     ],
     'data': [
         'views/res_users_portal_employee.xml',
         'views/hr_employee_user_link.xml',
         'views/hr_employee_attendance_location.xml',
-        'views/hr_work_location_geofence_views.xml',
         'views/portal_check_in_templates.xml',
     ],
 
-    "installable": True,
 
     'license': "Other proprietary",
 }
