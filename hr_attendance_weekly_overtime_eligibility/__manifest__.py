6e76d177f9a1f2edee55eab8421f55029655372f
rana-faris <erp-odoo@anabtawisweets.com>
2026-04-25 20:51:40 +0300

    [MOD]hr_attendance_weekly_overtime_eligibility

--- hr_attendance_weekly_overtime_eligibility/__manifest__.py
+++ hr_attendance_weekly_overtime_eligibility/__manifest__.py
@@ -15,20 +15,3 @@
     "installable": True,
     "application": False,
 }
-{
-    "name": "HR Attendance Weekly Overtime Eligibility",
-    "version": "19.0.1.0.0",
-    "summary": "Require minimum weekly worked hours before overtime approval",
-    "author": "Custom",
-    "license": "LGPL-3",
-    "depends": [
-        "hr_attendance",
-    ],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/res_config_settings_views.xml",
-        "views/hr_attendance_views.xml",
-    ],
-    "installable": True,
-    "application": False,
-}
