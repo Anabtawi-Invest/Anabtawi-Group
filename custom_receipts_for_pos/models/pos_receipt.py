098c7571699db4f2d03b33a7ad07048ef2ea12cf
Mohammad Nabil Abukhader <MNabil@anabtawisweets.com>
2026-04-27 11:40:34 +0300

    Update pos_receipt.py

--- custom_receipts_for_pos/models/pos_receipt.py
+++ custom_receipts_for_pos/models/pos_receipt.py
@@ -1,21 +1,17 @@
-# -*- coding: utf-8 -*-
 from odoo import api, fields, models
-
 
 class PosReceipt(models.Model):
     _name = "pos.receipt"
     _description = "POS Receipts"
-    _inherit = ["pos.load.mixin"]  # ✅ IMPORTANT for Odoo 19 POS loading
+    _inherit = ["pos.load.mixin"]
 
-    name = fields.Char(string="Name", help="Name of the pos receipt")
-    design_receipt = fields.Text(string="Receipt XML", help="Add your customised receipts for pos")
+    name = fields.Char()
+    design_receipt = fields.Text()
 
     @api.model
     def _load_pos_data_domain(self, data, config):
-        """Load all receipt designs to POS."""
         return []
 
     @api.model
     def _load_pos_data_fields(self, config):
-        """Fields to be read and sent to POS UI."""
         return ["id", "name", "design_receipt"]
