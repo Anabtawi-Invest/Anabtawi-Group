ff842037d7fb77e6968d877f600ff5afc46141c6
Mohammad Nabil Abukhader <MNabil@anabtawisweets.com>
2026-04-27 12:56:11 +0300

    Update pos_receipt.py

--- custom_receipts_for_pos/models/pos_receipt.py
+++ custom_receipts_for_pos/models/pos_receipt.py
@@ -1,12 +1,22 @@
+# -*- coding: utf-8 -*-
 from odoo import api, fields, models
+
 
 class PosReceipt(models.Model):
     _name = "pos.receipt"
-    _description = "POS Receipts"
-    _inherit = ["pos.load.mixin"]
+    _description = "POS Receipt Design"
 
-    name = fields.Char()
-    design_receipt = fields.Text()
+    name = fields.Char(
+        string="Name",
+        required=True,
+        help="Name of the POS receipt design.",
+    )
+
+    design_receipt = fields.Text(
+        string="Receipt XML",
+        required=True,
+        help="OWL/QWeb receipt template used in the POS frontend.",
+    )
 
     @api.model
     def _load_pos_data_domain(self, data, config):
