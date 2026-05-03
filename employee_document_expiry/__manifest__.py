0d0878cab4f9673b5481393e0b2291efb521c623
rana-faris <erp-odoo@anabtawisweets.com>
2026-05-03 20:32:01 +0300

    [MOD]expiry_date

---
+++ employee_document_expiry/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Employee Document Expiry & HR Email",
+    "version": "19.0.2.1.0",
+    "category": "Human Resources",
+    "summary": "Expiry dates beside HR document uploads; email HR Managers when overdue.",
+    "license": "LGPL-3",
+    # SIM / internet uploads come from Enterprise hr_payroll (used by e.g. l10n_be_hr_contract_salary).
+    "depends": ["hr", "mail", "hr_payroll"],
+    "data": [
+        "data/document_expiry_cron.xml",
+        "views/hr_employee_document_expiry_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
