300427daf21cdfe267777ce5611643abdcbcf1df
Ameen_Anabtawi <info@anabtawisweets.com>
2026-05-05 11:55:36 +0300

    Update __manifest__.py

--- purchase_confirm_approval/__manifest__.py
+++ purchase_confirm_approval/__manifest__.py
@@ -1,1 +1,21 @@
-
+{
+    "name": "Purchase Confirm Dynamic Approval",
+    "version": "19.0.1.0.0",
+    "category": "Purchase",
+    "by":"Anabtawi Group",
+    "summary": "Dynamic approval stages for Purchase Order confirmation",
+    "depends": [
+        "purchase",
+        "mail",
+    ],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/purchase_confirm_approval_views.xml",
+        "views/purchase_order_views.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
