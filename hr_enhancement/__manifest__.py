93eb44481f1141004fef0a8d80cdb981b204ae7f
rana-faris <erp-odoo@anabtawisweets.com>
2026-05-05 22:47:29 +0300

    [MOD]annual_leaves_days

--- hr_enhancement/__manifest__.py
+++ hr_enhancement/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     "name": "HR Enhancement",
-    "version": "19.0.1.2.1",
+    "version": "19.0.1.2.2",
     "category": "Human Resources",
     "summary": "Time off, attendance card PDF, regulated employee documents",
     "depends": ["hr_holidays", "hr_attendance", "mail"],
     "data": [
         "security/ir.model.access.csv",
+        "views/hr_employee_personal_views.xml",
         "views/hr_employee_document_views.xml",
         "data/employee_document_cron.xml",
         "reports/attendance_card_report.xml",
