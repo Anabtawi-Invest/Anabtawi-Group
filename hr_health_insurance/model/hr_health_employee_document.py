ce6a2959fc3a913f503c5f58223a4468eb52b43d
rana-faris <erp-odoo@anabtawisweets.com>
2026-05-15 15:52:04 +0300

    [MOD]document

--- hr_health_insurance/model/hr_health_employee_document.py
+++ hr_health_insurance/model/hr_health_employee_document.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
 
 
 class HrHealthEmployeeDocument(models.Model):
@@ -23,6 +25,7 @@ class HrHealthEmployeeDocument(models.Model):
     )
     document_file = fields.Binary(string='File', attachment=True)
     document_filename = fields.Char(string='Filename')
+    probation_start_date = fields.Date(string='Probation Start Date')
     expiry_date = fields.Date(string='Expiry Date')
 
     _sql_constraints = [
@@ -32,3 +35,38 @@ class HrHealthEmployeeDocument(models.Model):
             'This document type is already attached to the employee.',
         ),
     ]
+
+    @api.onchange('document_type_id', 'probation_start_date')
+    def _onchange_probation_dates(self):
+        self._sync_probation_expiry_date()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        if not self.env.context.get('skip_probation_expiry_sync'):
+            records._sync_probation_expiry_date()
+        return records
+
+    def write(self, vals):
+        result = super().write(vals)
+        if (
+            not self.env.context.get('skip_probation_expiry_sync')
+            and {'document_type_id', 'probation_start_date'} & set(vals)
+        ):
+            self._sync_probation_expiry_date()
+        return result
+
+    def _sync_probation_expiry_date(self):
+        probation_docs = self.filtered('document_type_id.is_probation_document')
+        for document in probation_docs:
+            expected_expiry = (
+                document.probation_start_date + relativedelta(months=3)
+                if document.probation_start_date else False
+            )
+            if document.expiry_date != expected_expiry:
+                if document._origin and document._origin.id:
+                    super(HrHealthEmployeeDocument, document.with_context(skip_probation_expiry_sync=True)).write({
+                        'expiry_date': expected_expiry,
+                    })
+                else:
+                    document.expiry_date = expected_expiry
