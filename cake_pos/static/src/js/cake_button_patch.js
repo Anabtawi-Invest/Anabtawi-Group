c663dd95d259494b313d12e4f6bc0ee9034603d7
Anabtawi-group <malnsour@anabtawisweets.com>
2026-05-20 11:01:47 +0300

    Patch ActionpadWidget to add Custom Cake button

--- cake_pos/static/src/js/cake_button_patch.js
+++ cake_pos/static/src/js/cake_button_patch.js
@@ -1,18 +1,11 @@
 /** @odoo-module **/
 
-/**
- * Patch ProductScreen to add the Custom Cake button.
- *
- * [J6] Odoo 17+ uses patch(Class, mixin) NOT patch(Class.prototype, mixin)
- */
-
 import { patch } from "@web/core/utils/patch";
-import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
-import { useService } from "@web/core/utils/hooks";
+import { ActionpadWidget } from "@point_of_sale/app/screens/product_screen/actionpad_widget/actionpad_widget";
 import { CustomCakePopup } from "./cake_popup";
 
-patch(ProductScreen, {
+patch(ActionpadWidget, {
     async openCustomCakePopup() {
-        await this.popup.add(CustomCakePopup, {});  // this.popup already exists
+        await this.popup.add(CustomCakePopup, {});
     },
 });
