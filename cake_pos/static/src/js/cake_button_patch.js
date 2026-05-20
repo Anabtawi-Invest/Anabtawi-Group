b10c6cc913c5d530fed69be2cb7a159b7ec6da5a
Anabtawi-group <malnsour@anabtawisweets.com>
2026-05-20 10:47:42 +0300

    Update cake_button_patch.js

--- cake_pos/static/src/js/cake_button_patch.js
+++ cake_pos/static/src/js/cake_button_patch.js
@@ -12,12 +12,7 @@ import { useService } from "@web/core/utils/hooks";
 import { CustomCakePopup } from "./cake_popup";
 
 patch(ProductScreen, {
-    setup() {
-        super.setup(...arguments);
-        this.popup = useService("popup");
-    },
-
     async openCustomCakePopup() {
-        await this.popup.add(CustomCakePopup, {});
+        await this.popup.add(CustomCakePopup, {});  // this.popup already exists
     },
 });
