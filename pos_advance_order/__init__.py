bf579202146f25e6a5ed2f5858c00674059eb60a
Anabtawi-group <malnsour@anabtawisweets.com>
2026-05-20 11:48:32 +0300

    Add files via upload

--- pos_advance_order/__init__.py
+++ pos_advance_order/__init__.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-
 from . import models
-from . import wizard
-from . import controllers
-
--- pos_advance_order/__manifest__.py
+++ pos_advance_order/__manifest__.py
@@ -1,51 +1,24 @@
-# -*- coding: utf-8 -*-
 {
-    "name": "       POS Advance Order-two cashiers ",
-    "author":"Anabtawi",
-    'version': '19.0.2.0.27',
+    "name": "POS Advance Order",
+    "version": "19.0.1.0.0",
     "category": "Point of Sale",
-    "summary": "Create and manage advance orders for POS pickup",
-    "license": "LGPL-3",
-    "depends": [
-        "point_of_sale",
-        "product",
-        "account",
-        "hr",
-        "employee_request",
-        "mail",
-    ],
+    "summary": "Advance order screen in POS that captures extra fields and saves them on the POS order.",
+    "author": "Softobia",
+    "website": "https://softobia.com",
+    "depends": ["point_of_sale"],
     "data": [
         "security/ir.model.access.csv",
-        "data/sequence.xml",
-        "views/pos_advance_order_views.xml",
-        "views/pos_advance_order_employee_pricelist_wizard_views.xml",
-        "views/pos_config_views.xml",
-        "views/res_partner_views.xml",
-        "views/pos_order.xml",
-        "views/pos_pledge_views.xml",
-        "views/product_pledge_views.xml",
-        "views/pos_advance_discount_views.xml",
-
-        "report/pos_advance_order_report.xml",
-        "report/pos_advance_order_receipt.xml",
-        "report/pos_advance_order_full_receipt.xml",
-
     ],
     "assets": {
-        "point_of_sale._assets_pos": [
-            "pos_advance_order/static/src/app/screens/product_screen/payment_button_visibility.xml",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/advance_order_receipt.js",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/advance_order_receipt.xml",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/advance_order_form_popup.js",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/advance_order_form_popup.xml",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/complete_advance_order_popup.js",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/complete_advance_order_popup.xml",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/advance_order_button.js",
-            "pos_advance_order/static/src/app/screens/product_screen/control_buttons/advance_order_button/advance_order_button.xml",
+        "point_of_sale.assets": [
+            "pos_advance_order_html/static/src/js/advance_order_button.js",
+            "pos_advance_order_html/static/src/js/advance_order_screen.js",
+            "pos_advance_order_html/static/src/js/order_export_patch.js",
+            "pos_advance_order_html/static/src/xml/advance_order_templates.xml",
+            "pos_advance_order_html/static/src/css/advance_order.css",
         ],
     },
+    "license": "LGPL-3",
     "installable": True,
-    "application": False,
-    "auto_install": False,
+    "application": False
 }
-
