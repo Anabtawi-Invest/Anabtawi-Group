8bada2792c7190f7d113652f6079bca94eb63f5d
Anabtawi-group <malnsour@anabtawisweets.com>
2026-05-20 12:20:33 +0300

    Update cake_button_patch.js

--- cake_pos/static/src/js/cake_button_patch.js
+++ cake_pos/static/src/js/cake_button_patch.js
@@ -1,29 +1,11 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { useService } from "@web/core/utils/hooks";
-import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { patch } from "@web/core/utils/patch";
 import { ActionpadWidget } from "@point_of_sale/app/screens/product_screen/actionpad_widget/actionpad_widget";
 import { CustomCakePopup } from "./cake_popup";
 
-export class CakeButton extends Component {
-    static template = "cake_pos.CakeButton";
-    static props = {};
-
-    setup() {
-        this.popup = useService("popup");
-        this.pos = usePos();
-    }
-
-    async openCakePopup() {
-        await this.popup.add(CustomCakePopup, {});
-    }
-}
-
 patch(ActionpadWidget, {
-    components: {
-        ...ActionpadWidget.components,
-        CakeButton,
+    async openCustomCakePopup() {
+        await this.popup.add(CustomCakePopup, {});
     },
 });
