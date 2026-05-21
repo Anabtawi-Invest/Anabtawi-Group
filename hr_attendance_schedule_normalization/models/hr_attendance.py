c4889833aa16574762db6bfccc3d8206a8568b87
rana-faris <erp-odoo@anabtawisweets.com>
2026-05-21 08:16:41 +0300

    [MOD]hr_normalization

--- hr_attendance_schedule_normalization/models/hr_attendance.py
+++ hr_attendance_schedule_normalization/models/hr_attendance.py
@@ -1,6 +1,4 @@
 from datetime import timedelta
-from functools import reduce
-from math import gcd
 
 import pytz
 
@@ -78,76 +76,63 @@ class HrAttendance(models.Model):
 
     def _normalize_check_in_time(self, employee, check_in_dt):
         tz = self._get_employee_timezone(employee)
-        shift_interval = self._get_employee_schedule_interval(
+        day_bounds = self._get_employee_schedule_day_bounds(
             employee=employee,
             reference_dt=check_in_dt,
             check_in_dt=check_in_dt,
         )
-        if not shift_interval:
+        if not day_bounds:
             return check_in_dt
 
-        shift_start, shift_end = shift_interval
+        day_start, _day_end = day_bounds
         check_in_local = self._convert_utc_naive_to_tz(check_in_dt, tz)
-        if check_in_local <= shift_start:
+        if check_in_local >= day_start:
             return check_in_dt
-
-        rounding_minutes = self._get_schedule_rounding_minutes(employee.resource_calendar_id)
-        normalized_local = self._round_datetime_up(
-            check_in_local,
-            anchor_dt=shift_start,
-            rounding_minutes=rounding_minutes,
-        )
-        normalized_local = min(normalized_local, shift_end)
-        return normalized_local.astimezone(pytz.UTC).replace(tzinfo=None)
+        return day_start.astimezone(pytz.UTC).replace(tzinfo=None)
 
     def _normalize_check_out_time(self, employee, check_out_dt, check_in_dt=None):
         tz = self._get_employee_timezone(employee)
-        shift_interval = self._get_employee_schedule_interval(
+        day_bounds = self._get_employee_schedule_day_bounds(
             employee=employee,
             reference_dt=check_out_dt,
             check_in_dt=check_in_dt,
         )
-        if not shift_interval:
+        if not day_bounds:
             return check_out_dt
 
-        _shift_start, shift_end = shift_interval
+        _day_start, day_end = day_bounds
         check_out_local = self._convert_utc_naive_to_tz(check_out_dt, tz)
-        if check_out_local <= shift_end:
+        if check_out_local <= day_end:
             return check_out_dt
 
-        normalized_check_out = shift_end.astimezone(pytz.UTC).replace(tzinfo=None)
+        normalized_check_out = day_end.astimezone(pytz.UTC).replace(tzinfo=None)
         if check_in_dt and normalized_check_out < check_in_dt:
             return check_in_dt
         return normalized_check_out
 
-    def _get_employee_schedule_interval(self, employee, reference_dt, check_in_dt=None):
+    def _get_employee_schedule_day_bounds(self, employee, reference_dt, check_in_dt=None):
         if not employee.resource_calendar_id:
             return False
 
         tz = self._get_employee_timezone(employee)
         pivot_dt = check_in_dt or reference_dt
         pivot_local = self._convert_utc_naive_to_tz(pivot_dt, tz)
-        interval_items = self._get_employee_work_intervals(employee, pivot_local)
+        day_start_local = pivot_local.replace(hour=0, minute=0, second=0, microsecond=0)
+        day_end_local = day_start_local + timedelta(days=1)
+        interval_items = self._get_employee_work_intervals(
+            employee,
+            day_start_local,
+            day_end_local,
+        )
         if not interval_items:
             return False
 
-        containing_index = self._find_containing_interval_index(interval_items, pivot_local)
-        if containing_index is None:
-            if check_in_dt:
-                return False
-            reference_local = self._convert_utc_naive_to_tz(reference_dt, tz)
-            previous_intervals = [item for item in interval_items if item[0] <= reference_local]
-            if not previous_intervals:
-                return False
-            latest_interval = previous_intervals[-1]
-            return latest_interval[0], latest_interval[1]
+        day_first_start = min(item[0] for item in interval_items)
+        day_last_end = max(item[1] for item in interval_items)
+        return day_first_start, day_last_end
 
-        return self._expand_contiguous_shift(interval_items, containing_index)
-
-    def _get_employee_work_intervals(self, employee, pivot_local_dt):
+    def _get_employee_work_intervals(self, employee, interval_start_local, interval_end_local):
         calendar = employee.resource_calendar_id
-        interval_start_local = pivot_local_dt - timedelta(days=1)
-        interval_end_local = pivot_local_dt + timedelta(days=2)
         interval_start_utc = interval_start_local.astimezone(pytz.UTC)
         interval_end_utc = interval_end_local.astimezone(pytz.UTC)
 
@@ -161,58 +146,6 @@ class HrAttendance(models.Model):
         if not employee_intervals:
             return []
         return sorted(employee_intervals._items, key=lambda item: item[0])
-
-    def _find_containing_interval_index(self, interval_items, dt_localized):
-        for index, (start_dt, end_dt, _attendance_line) in enumerate(interval_items):
-            if start_dt <= dt_localized <= end_dt:
-                return index
-        return None
-
-    def _expand_contiguous_shift(self, interval_items, initial_index):
-        shift_start, shift_end, _line = interval_items[initial_index]
-        # Overnight shifts are commonly split into adjacent intervals across midnight.
-        # Merge contiguous intervals to treat them as one logical shift.
-        max_allowed_gap = timedelta(seconds=1)
-
-        previous_index = initial_index - 1
-        while previous_index >= 0:
-            previous_start, previous_end, _line = interval_items[previous_index]
-            if shift_start - previous_end > max_allowed_gap:
-                break
-            shift_start = previous_start
-            previous_index -= 1
-
-        next_index = initial_index + 1
-        while next_index < len(interval_items):
-            next_start, next_end, _line = interval_items[next_index]
-            if next_start - shift_end > max_allowed_gap:
-                break
-            shift_end = next_end
-            next_index += 1
-
-        return shift_start, shift_end
-
-    def _get_schedule_rounding_minutes(self, calendar):
-        work_attendance_lines = calendar.attendance_ids.filtered(
-            lambda line: not line.display_type and line.day_period != "lunch"
-        )
-        minute_marks = []
-        for line in work_attendance_lines:
-            minute_marks.append(int(round(line.hour_from * 60)))
-            minute_marks.append(int(round(line.hour_to * 60)))
-        minute_marks = [minute_mark for minute_mark in minute_marks if minute_mark > 0]
-        if not minute_marks:
-            return 60
-        # Derive a stable rounding step (e.g. 15/30/60 min) from schedule boundaries.
-        return reduce(gcd, minute_marks) or 60
-
-    def _round_datetime_up(self, target_dt, anchor_dt, rounding_minutes):
-        rounding_seconds = max(1, int(rounding_minutes)) * 60
-        delta_seconds = (target_dt - anchor_dt).total_seconds()
-        if delta_seconds <= 0:
-            return anchor_dt
-        rounded_steps = int((delta_seconds + rounding_seconds - 1) // rounding_seconds)
-        return anchor_dt + timedelta(seconds=rounded_steps * rounding_seconds)
 
     def _get_employee_timezone(self, employee):
         timezone_name = employee._get_tz() or employee.tz or employee.resource_id.tz or "UTC"
