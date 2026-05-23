1b2bcacc43b257a824684925c31ed1a76d78a33d
rana-faris <erp-odoo@anabtawisweets.com>
2026-05-23 13:41:51 +0300

    [MOD]Exclusive Payment Method

--- pos_exclusive_payment_method/static/src/app/screens/payment_screen/payment_screen.js
+++ pos_exclusive_payment_method/static/src/app/screens/payment_screen/payment_screen.js
@@ -5,26 +5,29 @@ import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 
-const EXCLUSIVE_PAYMENT_METHOD_MESSAGE = "هذه البيمنت مثود لا يمكنك اختيار بيمنت مثود أخرى معها";
-
 patch(PaymentScreen.prototype, {
     async addNewPaymentLine(paymentMethod) {
-        const hasDifferentSelectedMethod = this.paymentLines.some(
-            (paymentLine) => paymentLine.payment_method_id.id !== paymentMethod.id
-        );
-        const hasExclusiveMethodInExistingLines = this.paymentLines.some(
+        const existingExclusiveLine = this.paymentLines.find(
             (paymentLine) =>
                 paymentLine.payment_method_id.exclusive_payment_method &&
                 paymentLine.payment_method_id.id !== paymentMethod.id
         );
+        const hasDifferentSelectedMethod = this.paymentLines.some(
+            (paymentLine) => paymentLine.payment_method_id.id !== paymentMethod.id
+        );
+        const hasExclusiveMethodInExistingLines = Boolean(existingExclusiveLine);
 
         if (
             (paymentMethod.exclusive_payment_method && hasDifferentSelectedMethod) ||
             hasExclusiveMethodInExistingLines
         ) {
+            const blockingPaymentMethodName =
+                paymentMethod.exclusive_payment_method && hasDifferentSelectedMethod
+                    ? paymentMethod.name
+                    : existingExclusiveLine?.payment_method_id?.name || paymentMethod.name;
             this.dialog.add(AlertDialog, {
                 title: _t("Error"),
-                body: EXCLUSIVE_PAYMENT_METHOD_MESSAGE,
+                body: `طريقة الدفع (${blockingPaymentMethodName}) لا يمكنك اختيار طريقة دفع أخرى معها`,
             });
             return false;
         }
