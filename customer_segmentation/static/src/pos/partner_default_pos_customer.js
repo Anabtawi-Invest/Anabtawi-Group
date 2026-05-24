7c3c1c64c1cb9ea090beb677453eba24e8dd16c3
rana-faris <erp-odoo@anabtawisweets.com>
2026-05-24 22:16:15 +0300

    [MOD]customer_segmentation

--- customer_segmentation/static/src/pos/partner_default_pos_customer.js
+++ customer_segmentation/static/src/pos/partner_default_pos_customer.js
@@ -1,7 +1,15 @@
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/services/pos_store";
+import { user } from "@web/core/user";
 
 patch(PosStore.prototype, {
+    async setup() {
+        await super.setup(...arguments);
+        this.customerSegmentationPosTeam = await user.hasGroup(
+            "customer_segmentation.group_pos_team"
+        );
+    },
+
     editPartnerContext() {
         return {
             ...super.editPartnerContext(...arguments),
--- customer_segmentation/static/src/pos/partner_list_filter.js
+++ customer_segmentation/static/src/pos/partner_list_filter.js
@@ -3,6 +3,9 @@ import { PartnerList } from "@point_of_sale/app/screens/partner_list/partner_lis
 
 patch(PartnerList.prototype, {
     getPartners(partners) {
+        if (!this.pos.customerSegmentationPosTeam) {
+            return super.getPartners(partners || []);
+        }
         const posCustomersOnly = (partners || []).filter(
             (partner) => partner && partner.is_pos_customer === true
         );
