e3c293bb5d706151f80e6174f2c9c011aae0b50c
rana-faris <erp-odoo@anabtawisweets.com>
2026-06-07 20:08:31 +0300

    [IMP] portal_check_in: add attendance enhancements

--- portal_check_in/__manifest__.py
+++ portal_check_in/__manifest__.py
@@ -17,7 +17,8 @@
         'views/hr_employee_user_link.xml',
         'views/hr_employee_attendance_location.xml',
         'views/portal_check_in_templates.xml',
-        'views/hr_work_location_geofence_views.xml'
+        'views/hr_work_location_geofence_views.xml',
+        'data/ir_cron_data.xml',
     ],
     'license': "Other proprietary",
 }
