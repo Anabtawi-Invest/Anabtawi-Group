cea6b18da4634601f241d606b7507a0211f8702f
rana-faris <erp-odoo@anabtawisweets.com>
2026-06-15 12:27:58 +0300

    [MOD]sale_fiscal_position_keep_price

--- sale_fiscal_position_keep_price/__manifest__.py
+++ sale_fiscal_position_keep_price/__manifest__.py
@@ -5,10 +5,15 @@
     "category": "Sales/Sales",
     "license": "LGPL-3",
     "author": "Anabtawi",
-    "depends": ["sale_management", "account"],
+    "depends": ["sale_management", "account", "point_of_sale"],
     "data": [
         "views/account_fiscal_position_views.xml",
     ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "sale_fiscal_position_keep_price/static/src/pos/fiscal_position_keep_price_patch.js",
+        ],
+    },
     "installable": True,
     "application": False,
 }
