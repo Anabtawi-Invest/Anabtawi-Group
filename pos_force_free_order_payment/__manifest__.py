70902065815f7692695c1964adfffcb9653cc39e
Anabtawi-Invest <info@anabtawisweets.com>
2026-06-17 15:03:23 +0300

    [ADD] pos_force_free_order_payment, Model

---
+++ pos_force_free_order_payment/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "POS Force Payment On Free Orders",
+    "summary": "Create a POS payment line for zero-total paid orders.",
+    "version": "19.0.1.0.0",
+    "category": "Point of Sale",
+    "author": "Custom",
+    "license": "LGPL-3",
+    "depends": ["point_of_sale"],
+    "installable": True,
+    "application": False,
+}
