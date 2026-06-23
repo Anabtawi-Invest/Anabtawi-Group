bd3812f5ff37fed31d2509f403b080ed2f66811e
rana-faris <erp-odoo@anabtawisweets.com>
2026-06-23 21:58:53 +0300

    [MOD]Internal Transfer Excel Report-Anabtawi

---
+++ internal_transfer_excel_report/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Internal Transfer Excel Report-Anabtawi',
+    'version': '1.1',
+    "author":"Anabtawi",
+    'depends': ['stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/wizard_view.xml',
+    ],
+    'installable': True,
+}
