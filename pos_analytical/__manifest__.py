5c64d0944e05dfec56b637faa1b81df603d58db5
rana-faris <erp-odoo@anabtawisweets.com>
2026-06-27 14:06:35 +0300

    [MOD]auth_force_password_change

--- pos_analytical/__manifest__.py
+++ pos_analytical/__manifest__.py
@@ -9,7 +9,7 @@
     "category": "Point Of Sale",
     "summary": "Link Analytic Account Configure Analytic Account Set Analytic Tags Analytic Journal Items, Analytic Journal Entries Point Of Sale Analytic Account Point Of Sale Analytic Tags POS Analytic Tags POS Analytic Account Analytic Account in POS POS Accounting Analysis POS Journal Analytic POS Configuration Analytic Account Odoo",
     "description": """This module helps configure the 'Analytic Account' in POS orders. You can set the analytic account based on each POS configuration. It automatically applies the 'Analytic Account' to journal entries and journal items, allowing you to analyze POS orders using analytic reports.""",
-    "version": "0.0.1",
+    "version": "0.0.2",
     "license": "OPL-1",
     "depends": ["point_of_sale", "analytic"],
     "application": True,
@@ -21,7 +21,7 @@
     ],
     'assets': {
         'point_of_sale._assets_pos': [
-            'sh_pos_analytic_tags/static/src/overrides/models.js'
+            'pos_analytical/static/src/overrides/models.js'
         ]
     },
     "images": ["static/description/background.png", ],
