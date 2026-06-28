0318fdaa0a36beb27c4bc44c5b283add0be38793
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-06-28 15:09:24 +0300

    Delete account_check_print directory

--- account_check_print/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import account_check_layout
-from . import account_check_print_history
-from . import account_journal
-from . import account_payment
-from . import ir_actions_report
-
--- account_check_print/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import check_reprint_wizard
-from . import check_void_wizard
-
