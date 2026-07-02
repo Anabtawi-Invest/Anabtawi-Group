d94b0e5c832d0748a0cd4b3edbc591b8ca18ac08
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-02 10:54:06 +0300

    Delete online_campaigns_discount directory

--- online_campaigns_discount/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import online_aggregator
-from . import online_campaign
-from . import online_campaign_report
-from . import online_campaign_settlement
-from . import pos_order
-from . import pos_session
