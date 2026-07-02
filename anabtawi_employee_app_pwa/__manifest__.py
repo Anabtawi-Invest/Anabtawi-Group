69b007b2669354bf287e098eff2e8dd722a53207
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-02 14:32:55 +0300

    Add files via upload

---
+++ anabtawi_employee_app_pwa/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Anabtawi Employee App PWA",
+    "version": "19.0.1.0.1",
+    "summary": "Installable Employee App PWA for Odoo 19",
+    "category": "Human Resources",
+    "author": "Anabtawi Group",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "web",
+        "hr",
+        "hr_attendance",
+        "anabtawi_mobile_api",
+    ],
+    "data": [
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
