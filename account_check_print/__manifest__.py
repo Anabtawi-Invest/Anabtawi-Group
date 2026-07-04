f0110a4afcec20b8f5e1b366e4dd88e25c4ff416
rana-faris <erp-odoo@anabtawisweets.com>
2026-07-04 14:47:49 +0300

    [MOD]account_check_print

--- account_check_print/__manifest__.py
+++ account_check_print/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Account Check Print",
     "summary": "Configurable multi-bank business check printing",
-    "version": "19.0.1.0.6",
+    "version": "19.0.1.0.7",
     "category": "Accounting/Accounting",
     "author": "Anabtawi Group",
     "license": "LGPL-3",
