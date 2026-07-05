91b6a7d6690607af5793986197dd67617e735e1c
Ameen_Anabtawi <info@anabtawisweets.com>
2026-07-05 13:24:04 +0300

    Create __manifest__.py

---
+++ factory_plan_category/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Factory Plan Category",
+    "version": "19.0.1.0.0",
+    "summary": "Adds Factory Plan Category to Product.",
+    "author": "Anabtawi Sweets",
+    "category": "Inventory",
+    "depends": ["product"],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
