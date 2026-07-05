e98471c1f1615d381310da1b9968f143c57ffc4d
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-05 11:09:19 +0300

    Add files via upload

---
+++ anabtawi_hr_performance_dashboard/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Anabtawi HR Performance Dashboard",
+    "summary": "ISO 30414 HR performance KPIs and dashboards for Anabtawi",
+    "version": "19.0.1.0.0",
+    "category": "Human Resources",
+    "author": "Anabtawi Group",
+    "license": "LGPL-3",
+    "depends": ["hr", "hr_contract", "hr_holidays", "hr_recruitment", "account", "mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/hr_kpi_data.xml",
+        "views/hr_kpi_views.xml",
+        "views/hr_kpi_menus.xml"
+    ],
+    "application": True,
+    "installable": True
+}
