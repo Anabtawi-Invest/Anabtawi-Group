b15a60005c3665b4929a503a53ef42498b9f930c
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-06 16:28:45 +0300

    Delete anabtawi_online_campaigns directory

--- anabtawi_online_campaigns/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Load concrete/table-backed models before SQL report models.
-# This ensures Odoo creates the POS line campaign columns before the SQL view init() runs.
-from . import online_aggregator
-from . import online_campaign
-from . import pos_order
-from . import pos_session
-from . import online_campaign_settlement
-from . import online_campaign_report
