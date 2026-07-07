01b45632f0acb092eb04c40a60277541017402fa
rana-faris <erp-odoo@anabtawisweets.com>
2026-07-07 22:53:43 +0300

    [ADD]internal_transfer_excel_report,add operation type

--- hr_attendance_overtime_approval_bridge/__manifest__.py
+++ hr_attendance_overtime_approval_bridge/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Attendance Overtime Approval Bridge",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "summary": "Require approved Approval Requests before validating extra hours",
     "author": "Custom",
     "license": "LGPL-3",
