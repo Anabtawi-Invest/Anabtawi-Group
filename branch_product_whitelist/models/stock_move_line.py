82c1a3950857e917fc01717ea765f09c26741866
rana-faris <erp-odoo@anabtawisweets.com>
2026-07-08 22:35:56 +0300

    [MOD]stock_barcode_demand_qty

--- branch_product_whitelist/models/stock_move_line.py
+++ branch_product_whitelist/models/stock_move_line.py
@@ -1,5 +1,6 @@
 from odoo import api, fields, models
 from odoo.exceptions import UserError
+from odoo.fields import Domain
 
 GROUP_XMLID = "branch_product_whitelist.group_branch_restricted_product_selection"
 ERROR_MSG = "This product is not allowed for branch internal transfers."
@@ -8,6 +9,8 @@ ERROR_MSG = "This product is not allowed for branch internal transfers."
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
+    branch_product_id_domain = fields.Binary(compute="_compute_branch_product_id_domain")
+
     @api.model
     def _branch_product_domain(self):
         return self.env["stock.move"]._branch_product_domain()
@@ -15,6 +18,19 @@ class StockMoveLine(models.Model):
     product_id = fields.Many2one(
         domain=lambda self: self._branch_product_domain(),
     )
+
+    @api.depends("company_id")
+    def _compute_branch_product_id_domain(self):
+        for line in self:
+            domain = Domain([
+                ("type", "=", "consu"),
+                "|",
+                ("company_id", "=", False),
+                ("company_id", "parent_of", line.company_id.id or line.env.company.id),
+            ])
+            if line.env.user.has_group(GROUP_XMLID):
+                domain &= Domain("product_tmpl_id.branch_allowed", "=", True)
+            line.branch_product_id_domain = domain
 
     def _check_branch_product_allowed(self):
         if not self.env.user.has_group(GROUP_XMLID):
