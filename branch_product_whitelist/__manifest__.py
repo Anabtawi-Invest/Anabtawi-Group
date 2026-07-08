3235bd112e0297c147ed4ece06ce9804ad2f5ffb
rana-faris <erp-odoo@anabtawisweets.com>
2026-07-08 22:25:03 +0300

    stock_barcode_demand_qty

--- branch_product_whitelist/__manifest__.py
+++ branch_product_whitelist/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Branch Product Whitelist for Internal Transfers-Ameen-Anabtawi",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.2.0",
     "category": "Inventory",
     "depends": ["stock", "product", "stock_picking_catalog", "stock_barcode"],
     "author":"Anabtawi",
@@ -9,7 +9,7 @@
         "security/ir.model.access.csv",
         "views/menu.xml",
         "views/product_template_views.xml",
-        
+        "views/stock_move_line_views.xml",
     ],
     "installable": True,
     "application": False,
---
+++ branch_product_whitelist/models/product_product.py
@@ -0,0 +1,21 @@
+from odoo import api, models
+
+from .stock_move_line import GROUP_XMLID
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    @api.model
+    def _get_branch_whitelist_extra_domain(self):
+        if not self.env.user.has_group(GROUP_XMLID):
+            return []
+        ctx = self.env.context
+        if ctx.get("default_picking_id") or ctx.get("active_model") == "stock.move.line":
+            return [("product_tmpl_id.branch_allowed", "=", True)]
+        return []
+
+    @api.model
+    def name_search(self, name="", args=None, operator="ilike", limit=100):
+        args = list(args or []) + self._get_branch_whitelist_extra_domain()
+        return super().name_search(name, args, operator, limit)
