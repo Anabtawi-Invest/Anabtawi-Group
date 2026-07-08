8eb9f33e541b038945b3154521bd1268f2a25cf0
rana-faris <erp-odoo@anabtawisweets.com>
2026-07-08 22:29:14 +0300

    [MOD]stock_barcode_demand_qty

--- branch_product_whitelist/__manifest__.py
+++ branch_product_whitelist/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Branch Product Whitelist for Internal Transfers-Ameen-Anabtawi",
-    "version": "19.0.1.2.0",
+    "version": "19.0.1.3.0",
     "category": "Inventory",
     "depends": ["stock", "product", "stock_picking_catalog", "stock_barcode"],
     "author":"Anabtawi",
