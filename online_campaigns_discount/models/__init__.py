4ef47356f4a678b74eb597294ec089b4f42b7ee6
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-12 15:30:20 +0300

    Add files via upload

---
+++ online_campaigns_discount/models/__init__.py
@@ -0,0 +1,6 @@
+from . import online_aggregator
+from . import online_campaign
+from . import pos_order
+from . import pos_session
+from . import online_campaign_settlement
+from . import online_campaign_report
