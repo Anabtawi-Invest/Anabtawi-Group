b44e540ef12107bb45407b876fbc9ee47f24baf3
Anabtawi-Invest <info@anabtawisweets.com>
2026-07-13 12:56:01 +0300

    [ADD] cost field to the pos order report

--- pos_report_cost/__manifest__.py
+++ pos_report_cost/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'POS Order Report - Cost',
-    'version': '19.0.1.0.0',
+    'version': '19.0.2.0.0',
     'category': 'Sales/Point of Sale',
     'summary': 'Adds the product Cost (standard_price) to the POS Orders report',
     'depends': ['point_of_sale'],
