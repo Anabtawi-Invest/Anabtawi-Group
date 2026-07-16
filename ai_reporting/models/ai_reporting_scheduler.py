da826e8e9105dd09de80464e6b2589744ea52e54
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-16 20:34:30 +0300

    Delete ai_reporting directory

--- ai_reporting/models/ai_reporting_scheduler.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class AiReportingScheduler(models.Model):
-    _name = "ai.reporting.scheduler"
-    _description = "AI Reporting Scheduler"
-
-    def _cron_refresh_metadata(self):
-        return self.env["ai.reporting.discovery.service"].refresh_metadata(scan_addons=True, build_templates=False)
