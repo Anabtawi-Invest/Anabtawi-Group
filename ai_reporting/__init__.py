db2c60ef71574e4837fbea245b102f3abbf079ae
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-16 22:43:29 +0300

    Delete ai_reporting directory

--- ai_reporting/__init__.py
+++
@@ -1,8 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import hooks
-from . import models
-from . import services
-from . import providers
-from . import controllers
-from . import wizard
