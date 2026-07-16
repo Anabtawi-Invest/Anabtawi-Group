2f690e3b3e6c07513dfe39a8062a04bc1d508969
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-16 17:49:22 +0300

    Delete ai_reporting directory

--- ai_reporting/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import ai_reporting_memory
-from . import ai_reporting_request
-from . import ai_reporting_scheduler
-from . import ai_reporting_saved_report
-from . import res_config_settings
