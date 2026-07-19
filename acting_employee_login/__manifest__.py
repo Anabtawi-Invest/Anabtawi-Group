df84e87b4b0cb589155123a4f31f07501ffe3741
rana-faris <erp-odoo@anabtawisweets.com>
2026-07-19 23:07:01 +0300

    [MOD]Employee Login

--- acting_employee_login/__manifest__.py
+++ acting_employee_login/__manifest__.py
@@ -10,7 +10,7 @@ selected in Settings.
     """,
     'category': 'Hidden',
     'author': 'enbtawi',
-    'version': '1.4',
+    'version': '1.5',
     'depends': [
         'base_setup',
         'hr',
