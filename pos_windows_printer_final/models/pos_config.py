a8555bc4189c205ac9a959051d2c19e0da1cf24f
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-25 14:52:18 +0300

    Add files via upload

---
+++ pos_windows_printer_final/models/pos_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    direct_printer_id = fields.Many2one(
+        'printer.printer',
+        string='Direct Branch Printer',
+        help='Assigned thermal printer (Windows Host, LAN, USB, Bluetooth) for this POS session.'
+    )
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        params = super()._load_pos_data_fields(config_id)
+        params.append('direct_printer_id')
+        return params
