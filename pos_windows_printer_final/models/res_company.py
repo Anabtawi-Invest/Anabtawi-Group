e299c3dd49575f98854df509e22ad70c8324b1ef
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-25 14:18:40 +0300

    Add files via upload

---
+++ pos_windows_printer_final/models/res_company.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        fields = super()._load_pos_data_fields(config_id)
+        if isinstance(fields, (list, tuple)):
+            fields = list(fields)
+            if 'tax_calculation_rounding_method' not in fields:
+                fields.append('tax_calculation_rounding_method')
+            if 'currency_id' not in fields:
+                fields.append('currency_id')
+            return fields
+        elif isinstance(fields, set):
+            fields.add('tax_calculation_rounding_method')
+            fields.add('currency_id')
+            return fields
+        return fields
