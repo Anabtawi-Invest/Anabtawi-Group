8e46be24a439a1b800ae5b9fa10a071588ca1d65
Anabtawi-Invest <info@anabtawisweets.com>
2026-07-26 11:28:01 +0300

    [Upgrade] Version For Daily Sales Operation

--- pos_ameen_daily_operation/__manifest__.py
+++ pos_ameen_daily_operation/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Daily Pos Sales Report',
-    'version': '7.0',
+    'version': '12.0',
     'author': 'Ameen Arabiyat - Rana Faris',
     'category': 'Point of Sale',
     'summary': 'Daily operations summary report by POS branch',
