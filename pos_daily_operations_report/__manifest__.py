7b435bbf2fe2fa7bef2c63dcd55f3d6cf27a2bee
Anabtawi-Invest <info@anabtawisweets.com>
2026-07-26 09:08:08 +0300

    [MOD] Update Version POS Daily Operations Report

--- pos_daily_operations_report/__manifest__.py
+++ pos_daily_operations_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'POS Daily Operations Report',
-    'version': '2.0',
+    'version': '3.0',
     'author': 'Anabtawi',
     'category': 'Point of Sale',
     'summary': 'Daily operations summary report by POS branch',
