4e33c45334fff9458e7ae4af4b42315ebd0905b9
rana-faris <erp-odoo@anabtawisweets.com>
2026-07-26 23:02:31 +0300

    fixing

--- pos_pricelist_id/__manifest__.py
+++ pos_pricelist_id/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'POS Pricelist  id enhancement-anabtawi  ',
-    'version': '1.2',
+    'version': '1.1',
     'category': 'Point of Sale',
     'summary': 'Manage pledge (Rahn) scenarios with employees, delivery, and accounting',
     'description': """
-
+   
     """,
     'author': 'Enbtawi Sweet',
-    'depends': ['point_of_sale', 'pos_sale', 'account', 'hr', 'online_campaigns_discount'],
+    'depends': ['point_of_sale', 'pos_sale', 'account', 'hr'],
     'data': [
         "views/product_pricelist.xml",
         "views/pos_order_views.xml",
@@ -18,7 +18,6 @@
         "point_of_sale._assets_pos": [
             "pos_pricelist_id/static/src/pos/id_number_validation.js",
             "pos_pricelist_id/static/src/pos/receipt_header.xml",
-            "pos_pricelist_id/static/src/pos/aggregator_order_ref_popup.js",
         ],
     },
     'installable': True,
