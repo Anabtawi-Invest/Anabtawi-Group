f68819a821f97ec7cfeec2b3e9df02130e705843
Mohammed Alnsour <malnsour@anabtawisweets.com>
2026-07-27 16:21:56 +0300

    Add files via upload

---
+++ journal_entry_print/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Journal Entry PDF",
+    "summary": "Print complete journal entries from the existing Print menu",
+    "version": "19.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "report/journal_entry_report.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
