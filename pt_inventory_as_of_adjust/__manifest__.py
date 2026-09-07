acd779c177b72b2e60a8e69e38ab93b07e01d2ba
Anabtawi-Invest <info@anabtawisweets.com>
2026-09-07 11:44:16 +0300

    [ADD] inventory adjustment

--- pt_inventory_as_of_adjust/__manifest__.py
+++ pt_inventory_as_of_adjust/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "PT Inventory As-of Adjustment",
 <<<<<<< HEAD
-    "version": "19.0.1.0.5",
+    "version": "19.0.1.0.6",
     "category": "Inventory",
     "summary": "Import counted qty as of a past date, review, and apply in batches",
     "description": """
