ea0cad6af125870b8c1e6d371296931ceb635195
Mohammad Nabil Abukhader <MNabil@anabtawisweets.com>
2026-09-16 16:06:10 +0300

    Add files via upload

---
+++ payslip/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Custom Payslip Report",
+    "version": "19.0.1.1.0",
+    "summary": "Bilingual Arabic / English payslip with three salary sections (Conditional on Termination Clearance)",
+    "category": "Human Resources/Payroll",
+    "license": "LGPL-3",
+    "depends": ["hr_payroll"],
+    "data": ["views/report_payslip_templates.xml"],
+    "installable": True,
+    "application": False,
+}
