2f1522ef8d7a7914f9fa21d828b30f3abdda3195
Mohammad Nabil Abukhader <MNabil@anabtawisweets.com>
2026-09-16 19:19:52 +0300

    [ADD] Termination_Payslip

--- payslip/__manifest__.py
+++ payslip/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Custom Payslip Report",
-    "version": "19.0.1.13.0",
+    "version": "19.0.1.14.0",
     "summary": "Bilingual Arabic / English payslip with three salary sections (Conditional on Termination Clearance)",
     "category": "Human Resources/Payroll",
     "license": "LGPL-3",
